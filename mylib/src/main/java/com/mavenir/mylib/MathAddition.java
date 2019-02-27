6165db01d65d1ef81567ed32a44971615902ec4e
Manoj Behera <manoj.behera@mavenir.com>
2019-02-27 13:41:35 +0530

    Initial commit

---
+++ mylib/src/main/java/com/mavenir/mylib/MathAddition.java
@@ -0,0 +1,14 @@
+package com.mavenir.mylib;
+
+public class MathAddition {
+
+    /**
+     * method author by Manoj Behera
+     * @param x
+     * @param y
+     * @return
+     */
+    public int addition(int x, int y){
+        return (x+y);
+    }
+}
